1411686695204117e75bb0eca8b01cea91f89ef5
jacob richards <jacob.richards@discordapp.com>
2025-08-24 13:35:01 -0600

    fix asset names

--- src/configuration/assetPaths.ts
+++ src/configuration/assetPaths.ts
@@ -5,15 +5,14 @@
 import { AssetPaths } from '../types/gameAssets.types';
 
 export const ASSET_PATHS: AssetPaths = {
-  playerSpriteSheet: '/src/assets/sprites/lwalk.png',
-  homeBackground: '/src/assets/maps/lhome.png',
-  barrenBackground: '', // Use procedural barren background instead
-  dirtTile: '/src/assets/terrain/dirt_48.svg',
+  playerSpriteSheet: '/src/assets/sprites/walk.png',
+  homeBackground: '/src/assets/maps/home.png',
+  dirtTile: '/src/assets/terrain/dirt48.svg',
   seedSprite: '/src/assets/cursedLand/objectsSeparately/rock1Shadow11.png',
   plantSprites: {
     eye: '/src/assets/cursedLand/objectsSeparately/eyePlantShadow11.png',
-    tentacle: '/src/assets/cursedLand/objectsSeparately/tentaclePlantShadow11.png',
-    jaws: '/src/assets/cursedLand/objectsSeparately/jawsPlantShadow11.png',
+    tentacle: '/src/assets/cursedLand/objectsSeparately/tentacePlantShadow11.png',
+    jaws: '/src/assets/cursedLand/objectsSeparately/jawPlantShadow11.png',
     spike: '/src/assets/cursedLand/objectsSeparately/spikePlantShadow11.png',
   },
 };
--- src/types/gameAssets.types.ts
+++ src/types/gameAssets.types.ts
@@ -9,8 +9,6 @@ export interface GameAssets {
   playerSprite: HTMLImageElement;
   /** Home background image */
   homeBackground: HTMLImageElement;
-  /** Barren/alien background image */
-  barrenBackground: HTMLImageElement;
   /** Base terrain tile (dirt) */
   dirtTile: HTMLImageElement;
   /** Seed sprite for unmatured plants */
@@ -24,8 +22,6 @@ export interface AssetPaths {
   playerSpriteSheet: string;
   /** Path to home background */
   homeBackground: string;
-  /** Path to barren background */
-  barrenBackground: string;
   /** Path to dirt terrain tile */
   dirtTile: string;
   /** Path to seed sprite */
